8716e6ecb209ab0cc27c444e396e351b6b6bbb87
Bryan Keener <bkeener7@gmail.com>
2022-08-04 11:22:28 -0600

    Add hashes_1.rb

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,35 +1,45 @@
 
-Activity:
-
+#Activity:
+puts "Part 1: Foods"
+puts ""
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  "apples" => 23,
+  "grapes" => 507,
+  "eggs" => 48}
 
-# Write code that prints all of the 'keys' of the foods variable 
+
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
+puts foods.keys
+puts ""
 
-# Write code that prints all of the 'values' of the foods variable 
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+puts foods.values
+puts ""
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+puts foods.values[1]
+puts ""
 
-# Write code that adds a food to the foods hash. 
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+foods["cherries"] = 30
+puts foods
+puts ""
+puts ""
 
 
 #-------------------
-# Part 2: Email
+puts "Part 2: Email"
 #-------------------
-
+puts ""
 
 # Think about all the pieces of information associated with one single email in your inbox.
 # It has a sender, a subject, ...
@@ -37,22 +47,36 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+email_message = {
+  "subject_line" => "Project Update",
+  "sender" => "bstanley@bcorp.com",
+  "recipient" => "jsmith@megacorp.com",
+  "body" => "We finally finished!",
+  "attachments" => "big project.pdf"
+}
+
 # Write code that prints your email hash to the terminal.
 
+puts email_message
+puts ""
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the email hash 
+puts email_message.keys
+puts ""
+
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
 
+puts email_message.values
+puts ""
+puts ""
 
 #-------------------
-# Part 3: Many Emails - CHALLENGE!
+puts "Part 3: Many Emails - CHALLENGE!"
 #-------------------
-
+puts ""
 # LONG EXAMPLE:
 # Now that we've learned about Hashes AND Arrays, we can combine them.
 
@@ -74,7 +98,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -85,15 +109,44 @@ posts = [
 ]
 
 p posts
+puts ""
 p posts[0]
+puts ""
+puts ""
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
+puts "Part 3: More emails"
+puts ""
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+email_messages = [
+{
+  "subject_line" => "Project Update",
+  "sender" => "bstanley@gmail.com",
+  "recipient" => "jsmith@megacorp.com",
+  "body" => "We finally finished!",
+  "attachments" => "big project.pdf"
+},
+{
+  "subject_line" => "I forgot to do part of the project",
+  "sender" => "intern@bcorp.com",
+  "recipient" => "bstanley@bcorp.com",
+  "body" => "It was $2 margarita night at Chili's and I didn't remember. Sorry.",
+  "attachments" => "pleaseforgiveme.png"
+},
+{
+  "subject_line" => "You're fired",
+  "sender" => "bstanley@bcorp.com",
+  "recipient" => "intern@bcorp",
+  "body" => "Just get out",
+  "attachments" => "madface.jpeg"
+}
+]
+puts email_messages
